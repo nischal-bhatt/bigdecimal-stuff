b80bedb7abaf4653c7e63a7bb0ea199f161fec75
Nishy baby <nischal.bhatt.87@gmail.com>
2023-07-03 21:43:25 +0800

    first commit

---
+++ src/MainClass.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class MainClass {
+    public static void main(String[] args) {
+        double a = 2.3;
+        double b = 2.1;
+        double c = a-b;
+
+        System.out.println(c);
+
+        BigDecimal a1 = new BigDecimal(String.valueOf(a));
+        BigDecimal b1 = new BigDecimal(String.valueOf(b));
+        BigDecimal c1 = a1.subtract(b1);
+
+        System.out.println(c1.doubleValue());
+    }
+}
